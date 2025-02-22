388744e69e888af171550b7336c374408bdf0dfb
blueapple825 <103878370+blueapple825@users.noreply.github.com>
2025-02-22 14:11:35 +0900

    Add files via upload

---
+++ llama_cpp_wasm/get_llama_cpp_wasm.js
@@ -0,0 +1,10 @@
+import { LlamaCpp } from "llama.js";
+
+function getLlama(model, onModelLoaded, onMessageChunk, onComplete) {
+  return new LlamaCpp(
+    model,
+    onModelLoaded,
+    onMessageChunk,
+    onComplete,
+  );
+}
